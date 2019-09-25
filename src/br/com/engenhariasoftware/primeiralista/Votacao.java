31e9275f5c513d09f7bc50dde328d6fb58de750b
aluno <aluno@aluno-H310MHC2>
2019-09-25 08:11:34 -0300

    Primeiro commit

---
+++ src/br/com/engenhariasoftware/primeiralista/Votacao.java
@@ -0,0 +1,25 @@
+package br.com.engenhariasoftware.primeiralista;
+
+import java.util.Random;
+
+public class Votacao {
+
+	public static void main(String[] args) {
+		try {
+			int tempo = 60;
+			int chapa1 = 0, chapa2 = 0, branco = 0, nulo =  0;
+			Random random = new Random();
+			for (int i = 0; i < tempo; i++) {
+				Thread.sleep(1000);
+				System.out.println("Os votos estão senco computados.....");
+				
+			}
+			while(true) {
+				
+			}
+		}
+		
+
+	}
+
+}
